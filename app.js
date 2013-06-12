c8b6fe6589f72c8255f0423307a0275f7e28a864
Louis Chatriot <louis.chatriot@gmail.com>
2013-06-12 10:20:47 +0200

    Able to return the customer's stripe id from cache and the stripe api

--- app.js
+++ app.js
@@ -4,10 +4,14 @@
 
 var express = require('express')
   , http = require('http')
-  , config = require('./lib/config')
-  , expressServer
-  , middlewares = require('./lib/middlewares')
   , h4e = require('h4e')
+  , request = require('request')
+  , async = require('async')
+  , _ = require('underscore')
+  , config = require('./lib/config')
+  , middlewares = require('./lib/middlewares')
+  , db = require('./lib/db')
+  , expressServer
   ;
 
 
@@ -27,19 +31,64 @@ expressServer.use(express.cookieParser());
 expressServer.use(expressServer.router);
 
 
+
+/**
+ * Answer a Rapportive request
+ */
+function answerRapportive (req, response) {
+  response.status = response.status || 200;
+  req.res.send(200, req.query.callback + '(' +  JSON.stringify(response) + ')');
+}
+
+
 /**
  * Routes
  */
 expressServer.get('/', function (req, res, next) {
   var response = {};
 
-  console.log("===========");
-  console.log(req.query);
+  if (!req.query.email) { return answerRapportive(req, { status: 400, html: '' }); }
 
-  response.status = 200;
-  response.html = "<b>TEST</b>";
+  async.waterfall([
+    // Try to find the Stripe customer id in the nedb cache, and aske the Stripe API if we can't find it
+    // The next step will be executed only if a stripe customer id was found for this email address
+    // This way is not really optimal (we should perform a full synchro and not only for the given email address)
+    // but for the demo this is fine
+    function (cb) {
+      db.customers.findOne({ email: req.query.email }, function (err, customer) {
+        if (err) { return answerRapportive(req, { status: 500, html: '' }); }
 
-  res.send(200, req.query.callback + '(' +  JSON.stringify(response) + ')');
+        if (customer) { return cb(null, customer.stripeCustomerId); }
+
+        // No customer found, getting back the whole list
+        request.get({ uri: 'https://api.stripe.com/v1/customers?count=100', auth: { username: process.env.STRIPE_TEST_KEY } }, function (err, res, obj) {
+          if (err) { return answerRapportive(req, { status: 500, html: '' }); }
+
+          try {
+            obj = JSON.parse(obj)
+          } catch (e) {
+            return answerRapportive(req, { status: 400, html: '' });
+          }
+
+          obj = _.find(obj.data, function (c) { return c.email === req.query.email });
+          if (!obj) {
+            return answerRapportive(req, { status: 404, html: 'Stripe user not found' });
+          } else {
+            db.customers.insert({ email: req.query.email, stripeCustomerId: obj.id }, function (err) {
+              if (err) { return answerRapportive(req, { status: 500, html: '' }); }
+              return cb(null, obj.id);
+            });
+          }
+        });
+      });
+    }
+    // We got a customer id, let's give back some data about him
+    , function (stripeCustomerId, cb) {
+      response.status = 200;
+      response.html = "<b>STRIPE DATA</b><br>" + stripeCustomerId;
+      return answerRapportive(req, response);
+    }
+  ]);
 });
 
 
@@ -52,15 +101,19 @@ expressServer.launchServer = function (cb) {
     , self = this
     ;
 
-  self.apiServer = http.createServer(self);   // Let's not call it 'server' we never know if Express will want to use this variable!
+  db.initialize(function (err) {
+    if (err) { return callback(err); }
 
-  // Handle any connection error gracefully
-  self.apiServer.on('error', function () {
-    return callback("An error occured while launching the server, probably a server is already running on the same port!");
+    self.apiServer = http.createServer(self);   // Let's not call it 'server' we never know if Express will want to use this variable!
+
+    // Handle any connection error gracefully
+    self.apiServer.on('error', function () {
+      return callback("An error occured while launching the server, probably a server is already running on the same port!");
+    });
+
+    // Begin to listen. If the callback gets called, it means the server was successfully launched
+    self.apiServer.listen.apply(self.apiServer, [config.serverPort, callback]);
   });
-
-  // Begin to listen. If the callback gets called, it means the server was successfully launched
-  self.apiServer.listen.apply(self.apiServer, [config.serverPort, callback]);
 };
 
 
